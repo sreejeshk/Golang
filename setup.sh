d60142bed51c8e6db11fb82956aa318098c91b2a
sreejesh <ubuntu@ip-172-31-22-116.ec2.internal>
2018-11-09 15:57:41 +0000

    asdhasd da

--- setup.sh
+++ setup.sh
@@ -26,4 +26,4 @@ sudo sed -i '/#bind-address*/c\bind-address= 0.0.0.0' /etc/mysql/mysql.conf.d/my
 sudo service mysql restart
 cd /home/ubuntu/athul/Golang
 sudo docker build -t golang-demo:1.0 .
-sudo docker run -p 8080:8000 -e db_host=$pubIP django:1.0
+sudo docker run -p 8080:8000 -e db_host=$pubIP golang-demo:1.0
